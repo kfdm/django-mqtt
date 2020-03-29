3dfde7026680241415eeed4a9900d08dbebc6c22
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-03-29 19:16:06 +0900

    Fix decoding

--- vernemq.py
+++ vernemq.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 # https://docs.vernemq.com/plugindevelopment/webhookplugins#auth_on_register
 class Auth(APIView):
     def post(self, request):
-        data = json.loads(request.body)
+        data = json.loads(request.body.decode("utf-8"))
         if authenticate(request, username=data["username"], password=data["password"]):
             return JsonResponse({"result": "ok"})
         logger.error("Invalid login for %s", data["username"])

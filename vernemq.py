f99db6bc9089aa44f0adb305ae87fc6eb8bf20bd
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-03-29 19:10:11 +0900

    Initial vernemq auth path

---
+++ vernemq.py
@@ -0,0 +1,19 @@
+import json
+import logging
+
+from rest_framework.views import APIView
+
+from django.contrib.auth import authenticate
+from django.http import HttpResponseForbidden, JsonResponse
+
+logger = logging.getLogger(__name__)
+
+
+# https://docs.vernemq.com/plugindevelopment/webhookplugins#auth_on_register
+class Auth(APIView):
+    def post(self, request):
+        data = json.loads(request.body)
+        if authenticate(request, username=data["username"], password=data["password"]):
+            return JsonResponse({"result": "ok"})
+        logger.error("Invalid login for %s", data["username"])
+        return HttpResponseForbidden()

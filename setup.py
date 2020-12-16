d11ce1cf68103bfdb8203508c75cc137698702b0
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-12-16 20:45:02 +0900

    Add LICENSE

--- setup.py
+++ setup.py
@@ -3,9 +3,17 @@ from setuptools import find_packages, setup
 setup(
     name="django-mqtt",
     author="Paul Traylor",
-    url="http://github.com/kfdm/django-mqtt",
+    packages=find_packages(),
+    version="0.0.1",
+    license="MIT",
     install_requires=[
         "Django>=2.0",
         "paho-mqtt",
     ],
+    url="http://github.com/kfdm/django-mqtt",
+    project_urls={
+        "Issues": "http://github.com/kfdm/django-mqtt/issues",
+        "Source Code": "http://github.com/kfdm/django-mqtt",
+    },
+    keywords="django mqtt",
 )

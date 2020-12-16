ec4368c36fdfdc89de4c4a808d62fc93e3dd6a2b
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-12-16 20:49:05 +0900

    Update some classifiers

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
+"""
+MQTT tools for Django
+"""
+
 from setuptools import find_packages, setup
+
+readme_file = open("README.md", "rt").read()
 
 setup(
     name="django-mqtt",
     author="Paul Traylor",
-    packages=find_packages(),
+    packages=find_packages(exclude=["test"]),
     version="0.0.1",
-    license="MIT",
+    license="MIT License",
     install_requires=[
         "Django>=2.0",
         "paho-mqtt",
@@ -16,4 +22,19 @@ setup(
         "Source Code": "http://github.com/kfdm/django-mqtt",
     },
     keywords="django mqtt",
+    description=" ".join(__doc__.splitlines()).strip(),
+    long_description=readme_file,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
 )

b6b01be0c8dcff03f882405504ca2b9ee2e9cc85
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-10-17 15:37:33 +0900

    Allow toggling json debug or not

--- dmqtt/signals.py
+++ dmqtt/signals.py
@@ -10,16 +10,14 @@ connect = django.dispatch.Signal(providing_args=["userdata", "flags", "rc"])
 message = django.dispatch.Signal(providing_args=["userdata", "msg"])
 
 
-def topic(matcher, **extras):
+def topic(matcher, as_json=True, **extras):
     def wrap(func):
         def inner(msg, **kwargs):
             if fnmatch.fnmatch(msg.topic, matcher):
                 logger.debug("Matched %s for %s", matcher, func)
-                func(
-                    topic=msg.topic,
-                    data=json.loads(msg.payload.decode("utf8")),
-                    **kwargs
-                )
+                if as_json:
+                    kwargs["data"] = json.loads(msg.payload.decode("utf8"))
+                func(topic=msg.topic, **kwargs)
 
         message.connect(inner, **extras)
         return inner

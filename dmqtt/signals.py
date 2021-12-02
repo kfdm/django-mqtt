b23a7d3247cccbe7a350ed78d46cf60a1ebed06c
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-12-02 19:21:40 +0900

    Ensure we send the original msg to our handling function

--- dmqtt/signals.py
+++ dmqtt/signals.py
@@ -18,7 +18,7 @@ def topic(matcher, as_json=True, **extras):
                 logger.debug("Matched %s for %s", matcher, func)
                 if as_json:
                     kwargs["data"] = json.loads(msg.payload.decode("utf8"))
-                func(topic=msg.topic, **kwargs)
+                func(topic=msg.topic, msg=msg, **kwargs)
 
         message.connect(inner, **extras)
         return inner
@@ -36,7 +36,7 @@ def regex(pattern, *, as_json=True, **extras):
                 logger.debug("Matched %s for %s", match, func)
                 if as_json:
                     kwargs["data"] = json.loads(msg.payload.decode("utf8"))
-                func(topic=msg.topic, match=match, **kwargs)
+                func(topic=msg.topic, match=match, msg=msg, **kwargs)
 
         message.connect(inner, **extras)
         return inner

17f58007987e08e697e5b14a404e98b7c5c9b1f6
Paul Traylor <kungfudiscomonkey@gmail.com>
2022-06-16 19:03:51 +0900

    Switch our JSONEncoder to a decorator we can reuse

--- dmqtt/shortcuts.py
+++ dmqtt/shortcuts.py
@@ -1,6 +1,6 @@
 import json
 import uuid
-from functools import lru_cache
+from functools import lru_cache, wraps
 
 from paho.mqtt import publish
 
@@ -16,7 +16,23 @@ except ImportError:
     from django.core.serializers.json import DjangoJSONEncoder as JSONEncoder
 
 
-__all__ = ["single"]
+__all__ = ["single", "json_payload"]
+
+
+def json_payload(func):
+    """
+    Decorator to add json support to MQTT publish methods
+    """
+
+    @wraps(func)
+    def __inner(*args, **kwargs):
+        if "json" in kwargs:
+            data = kwargs.pop("json")
+            kwargs["payload"] = json.dumps(data, cls=JSONEncoder).encode("utf8")
+
+        return func(*args, **kwargs)
+
+    return __inner
 
 
 @lru_cache(maxsize=1)
@@ -24,11 +40,12 @@ def client_id():
     return (get_current_site(None).domain + "-%d" % uuid.uuid4().int)[:32]
 
 
+@json_payload
+@wraps(publish.single)
 def single(topic, **kwargs):
-    if "json" in kwargs:
-        data = kwargs.pop("json")
-        kwargs["payload"] = json.dumps(data, cls=JSONEncoder).encode("utf8")
-
+    """
+    Wrapped version of single, supporting AUTH and json payload
+    """
     if settings.MQTT_USER and settings.MQTT_PASS:
         kwargs["auth"] = {
             "username": settings.MQTT_USER,
